498694a115b4ef68fcfdadea5699a13dc68dc08b
thoyh <thoyh@VICTORIAYLEO>
2023-08-29 13:46:17 +0200

    Add project files.

---
+++ productbuyer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http.Headers;
+using System.Xml.Schema;
+
+Console.WriteLine("Ange en produkt");
+string produkt = Console.ReadLine();
+Console.WriteLine("Ange pris på produkten");
+double pris = double.Parse(Console.ReadLine());
+Console.WriteLine("Ange antal att köpa");
+int antal = int.Parse(Console.ReadLine());
+double total = antal * pris;
+
+Console.WriteLine($"{antal} {produkt} kostar {total}");
+
+
